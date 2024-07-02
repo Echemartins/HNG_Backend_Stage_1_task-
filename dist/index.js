a7127d01f1a3bd2ed9d2ac35f03f814edc7c88fc
Echemartins <echemartins47@gmail.com>
2024-07-02 10:44:07 +0100

    pushed disted folder

---
+++ dist/index.js
@@ -0,0 +1,65 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.get('/api/hello', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const visitorName = req.query.visitor_name;
+    const clientIp = req.socket.remoteAddress || req.ip;
+    const apiKey = process.env.OPENWEATHER_API_KEY;
+    try {
+        const locationResponse = yield axios_1.default.get('http://ip-api.com/json/');
+        const city = locationResponse.data.city;
+        const weatherResponse = yield axios_1.default.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+        const temp = weatherResponse.data.main.temp;
+        res.status(200).json({
+            client_ip: clientIp,
+            location: city,
+            greeting: `Hello, ${visitorName}!, the temperature is ${temp} degrees Celsius in ${city}`
+        });
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Unable to fetch location data' });
+    }
+}));
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
